377049b279aaa8396f806d1a7c4650b8e72cb346
fujikawa721 <ipod.fujikawa@gmail.com>
2024-12-20 23:25:59 +0900

    「文字列を変化させ、戻す」を追加

--- src/main.js
+++ src/main.js
@@ -4,3 +4,12 @@ BUTTON_CLICK_EVENT.addEventListener('click', () => {
   let object = document.getElementById('object');
   object.innerText = '変更しました';
 });
+
+const BUTTON_CLICK_EVENT2 = document.getElementById('change2');
+let changeText = "変化しました！";
+BUTTON_CLICK_EVENT2.addEventListener('click', () => {
+  let object = document.getElementById('object2');
+  let tempText = object.textContent;
+  object.innerText = changeText;
+  changeText = tempText;
+});
